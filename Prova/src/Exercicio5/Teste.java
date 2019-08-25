b9f6c1ae88e4b2d84893936b81a496128fc0728d
gabrielpatrola <almeidagabrielaugusto@gmail.com>
2019-08-24 20:32:45 -0400

    exercicio 5 concluido

--- Prova/src/Exercicio5/Teste.java
+++ Prova/src/Exercicio5/Teste.java
@@ -8,15 +8,11 @@ public class Teste {
         Normal normal = new Normal(10);
         CamaroteInferior inferior = new CamaroteInferior("Sala 1", 2, 10);
         CamaroteSuperior superior = new CamaroteSuperior(3, 2, 10);
-        
-        
+
         ingresso.imprimeValor();
         vip.imprime_ingresso_vip();
         normal.imprime_ingresso();
         inferior.imprime_localizacao();
         superior.imprime_valor_do_ingresso_camarote_vip_superior();
-        
-
     }
-
 }
